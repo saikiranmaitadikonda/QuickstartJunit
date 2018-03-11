8c59e863f589251631eaa9de1034234ea721e982
ravikanthtadikonda1979 <ravikanth.x.tadikonda@outlook.com>
2018-03-11 10:02:14 -0700

    formatted the string url

--- src/main/java/com/quickstart/HttpURLConnection.java
+++ src/main/java/com/quickstart/HttpURLConnection.java
@@ -3,7 +3,7 @@ package com.quickstart;
 public class HttpURLConnection {
 
     public static final String URL_TO_CONNECT = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/" +
-                                            "webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
+            "webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
 
 }
