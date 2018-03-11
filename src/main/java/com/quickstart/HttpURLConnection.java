568bc967ec6347dca27acc9546be501da7c75fe4
ravikanthtadikonda1979 <ravikanth.x.tadikonda@outlook.com>
2018-03-11 09:59:31 -0700

    Added string url

--- src/main/java/com/quickstart/HttpURLConnection.java
+++ src/main/java/com/quickstart/HttpURLConnection.java
@@ -2,5 +2,8 @@ package com.quickstart;
 
 public class HttpURLConnection {
 
+    public static final String URL_TO_CONNECT = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/" +
+                                            "webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
+
 
 }
